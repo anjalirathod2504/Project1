0fe6cd8cb9aabb50b38a1af67af9cc1b93fc7404
Venky8790 <93594741+Venky8790@users.noreply.github.com>
2022-03-28 14:52:50 +0530

    Update Git_Automation.bat

--- Git_Automation.bat
+++ Git_Automation.bat
@@ -1,3 +1,3 @@
 @echo off
-"C:\Users\DELL\AppData\Local\Programs\Python\Python39\python.exe" "C:\Repo2\Git_Automation\testCases\gitAutomationTestExecution.py"
+"C:\Users\Admin\AppData\Local\Programs\Python\Python310\python.exe" "C:\Repo2\Git_Automation\testCases\gitAutomationTestExecution.py"
 pause
